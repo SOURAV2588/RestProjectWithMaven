db349b9b04bbc77df86b14d59fa763b71ad1d824
SOURAV2588 <souravghosh358@gmail.com>
2018-12-06 08:19:30 +0530

    Initial commit

---
+++ src/main/java/com/sourav/RestProjectWithMaven/MessageResource.java
@@ -0,0 +1,23 @@
+package com.sourav.RestProjectWithMaven;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/messages")
+public class MessageResource {
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getMessage() {
+		return "Hello World";
+	}
+	
+	@POST
+	public String postMEssgae() {
+		return null;
+	}
+
+}
